623f09dc380db84ebd414f0ea426fc8e02e18b44
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-22 09:32:09 -0300

    Código completo

--- minfo.sh
+++ minfo.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
-echo "hello"
+
+# Verifica se exatamente um argumento foi passado
+if [ "$#" -ne 1 ]; then
+    echo "Uso: ./minfo.sh {hostname|uptime|disk|all}"
+    exit 1
+fi
+
+case "$1" in
+    hostname)
+        hostname
+        ;;
+    uptime)
+        uptime -p
+        ;;
+    disk)
+        df -h /
+        ;;
+    all)
+        hostname
+        uptime -p
+        df -h /
+        ;;
+    *)
+        echo "Uso: ./minfo.sh {hostname|uptime|disk|all}"
+        exit 1
+        ;;
+esac
+
